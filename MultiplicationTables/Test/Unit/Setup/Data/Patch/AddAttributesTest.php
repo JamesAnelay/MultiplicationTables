577f05c994a9e3653c4ae05b07b7c7140d8420c9
jamesanelay <jamesanelay@gmail.com>
2024-09-24 01:05:38 +0100

    Add empty test

---
+++ MultiplicationTables/Test/Unit/Setup/Data/Patch/AddAttributesTest.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace EdmondsCommerce\MultiplicationTables\Test\Unit\Setup\Data\Patch;
+
+use PHPUnit\Framework\TestCase;
+
+class AddAttributesTest extends TestCase
+{
+    public function test_nothing(){
+        return false;
+    }
+}
